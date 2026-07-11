0a056f745b6ab369bbcd6727367ca676655a773e
Dionathan Passos <devdionathanpassos@gmail.com>
2026-07-10 22:32:47 -0300

    feat(social): adicionado Social Service

--- src/main/java/com/dionathan/portfolio_api/social/SocialMapper.java
+++ src/main/java/com/dionathan/portfolio_api/social/SocialMapper.java
@@ -1,4 +1,51 @@
 package com.dionathan.portfolio_api.social;
 
-public class Social {
+import com.dionathan.portfolio_api.auth.User;
+import com.dionathan.portfolio_api.social.dto.SocialRequestDTO;
+import com.dionathan.portfolio_api.social.dto.SocialResponseDTO;
+import com.dionathan.portfolio_api.social.dto.SocialUpdateRequestDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocialMapper {
+
+    public Social toEntity(SocialRequestDTO requestDTO, User user) {
+        Social social = new Social();
+
+        social.setGithubUrl(requestDTO.githubUrl());
+        social.setLinkedinUrl(requestDTO.linkedinUrl());
+        social.setEmail(requestDTO.email());
+        social.setWebsite(requestDTO.website());
+        social.setUser(user);
+
+        return social;
+    }
+
+    public SocialResponseDTO fromEntity(Social social) {
+        return new SocialResponseDTO(
+                social.getId(),
+                social.getGithubUrl(),
+                social.getLinkedinUrl(),
+                social.getEmail(),
+                social.getWebsite()
+        );
+    }
+
+    public Social updateToEntity(SocialUpdateRequestDTO requestDTO, Social social) {
+
+        if(requestDTO.githubUrl() != null) {
+            social.setGithubUrl(requestDTO.githubUrl());
+        }
+        if(requestDTO.linkedinUrl() != null) {
+            social.setLinkedinUrl(requestDTO.linkedinUrl());
+        }
+        if(requestDTO.email() != null) {
+            social.setEmail(requestDTO.email());
+        }
+        if(requestDTO.website() != null) {
+            social.setWebsite(requestDTO.website());
+        }
+
+        return social;
+    }
 }
--- src/main/java/com/dionathan/portfolio_api/social/SocialService.java
+++ src/main/java/com/dionathan/portfolio_api/social/SocialService.java
@@ -1,4 +1,55 @@
 package com.dionathan.portfolio_api.social;
 
-public class Social {
+import com.dionathan.portfolio_api.auth.User;
+import com.dionathan.portfolio_api.exception.ResourceNotFoundException;
+import com.dionathan.portfolio_api.security.AuthenticatedUserService;
+import com.dionathan.portfolio_api.social.dto.SocialRequestDTO;
+import com.dionathan.portfolio_api.social.dto.SocialResponseDTO;
+import com.dionathan.portfolio_api.social.dto.SocialUpdateRequestDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SocialService {
+
+    private final AuthenticatedUserService authenticatedUserService;
+    private final SocialRepository socialRepository;
+    private final SocialMapper socialMapper;
+
+    @Transactional
+    public SocialResponseDTO create(SocialRequestDTO requestDTO) {
+        User user = authenticatedUserService.getAuthenticatedUser();
+
+        Social social = socialMapper.toEntity(requestDTO, user);
+        Social saved = socialRepository.save(social);
+
+        return socialMapper.fromEntity(saved);
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public SocialResponseDTO findByUser() {
+        User user = authenticatedUserService.getAuthenticatedUser();
+
+        Social social = socialRepository.findByUser(user)
+                .orElseThrow(() -> new ResourceNotFoundException("Contatos não encontrado."));
+
+        return socialMapper.fromEntity(social);
+    }
+
+    @Transactional
+    public SocialResponseDTO update(SocialUpdateRequestDTO requestDTO, Long id) {
+        User user = authenticatedUserService.getAuthenticatedUser();
+
+        Social social = socialRepository.findByIdAndUser(id, user)
+                .orElseThrow(() -> new ResourceNotFoundException("Contato não encontrado."));
+
+        social = socialMapper.updateToEntity(requestDTO, social);
+
+        return socialMapper.fromEntity(social);
+    }
 }
